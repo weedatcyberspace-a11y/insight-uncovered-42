e3a0a6023f6b562ee3c0e90fec79e745c56432f4
Cyber Space TechCyberspaceTech-dev <samzononditz@gmail.com>
2025-09-24 02:33:53 +0300

    NEW

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,12 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import Auth from "./pages/Auth";
-import ResetPassword from "./pages/ResetPassword";
-import CreateSurvey from "./pages/CreateSurvey";
-import Profile from "./pages/Profile";
-import AuthGuard from "./components/AuthGuard";
-import NotFound from "./pages/NotFound";
-
-const queryClient = new QueryClient();
-
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/reset-password" element={<ResetPassword />} />
-          <Route path="/create-survey" element={<AuthGuard><CreateSurvey /></AuthGuard>} />
-          <Route path="/profile" element={<AuthGuard><Profile /></AuthGuard>} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+const App = () => {
+  return (
+    <div style={{minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center', background: '#fff'}}>
+      <div style={{textAlign: 'center', color: '#444'}}>
+        <h1 style={{fontSize: 20, marginBottom: 8}}>Site removed</h1>
+        <p style={{margin: 0}}>This site has been cleared. Contact the administrator for more information.</p>
+      </div>
+    </div>
+  );
+};
 
 export default App;
